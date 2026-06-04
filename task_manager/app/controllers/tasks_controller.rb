bcd5b133b88626f54f2605793a7c88c110c77f86
Dec-x13 <smurfacc4anygame29@gmail.com>
2026-06-04 12:30:36 +0800

    Updated to handle new App Features

--- task_manager/app/controllers/tasks_controller.rb
+++ task_manager/app/controllers/tasks_controller.rb
@@ -2,4 +2,30 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path # Send back to homepage if successful
+    else
+      render :new, status: :unprocessable_entity # Show from again if it fails
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.expect(task: [ :title, :description ])
+  end
 end
