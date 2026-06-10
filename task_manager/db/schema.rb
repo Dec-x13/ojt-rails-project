1303f7e8ecafec5ab10e4c23b2c1e6a5bc9a9d6f
Dec-x13 <smurfacc4anygame29@gmail.com>
2026-06-10 13:35:27 +0800

    Generate User model with has_secure_password and enforce email uniqueness

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_08_071052) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_10_051031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,6 +26,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_08_071052) do
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 
