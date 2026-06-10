8d7936aa44084cab8a6c890cc7a51f22389d26b7
Dec-x13 <smurfacc4anygame29@gmail.com>
2026-06-10 20:46:46 +0800

    Add completed boolean column to tasks table

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_10_051031) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_10_123820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_10_051031) do
   end
 
   create_table "tasks", force: :cascade do |t|
+    t.boolean "completed", default: false, null: false
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
